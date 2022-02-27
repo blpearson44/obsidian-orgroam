d162f71dd3b7c1c4e94775b373ff5a7f1ab375f5
Ben Pearson <blpearson44@icloud.com>
2022-02-26 17:56:21 -0800

    implemented link and code block changes

--- transfer.py
+++ transfer.py
@@ -2,8 +2,16 @@
 """Convert documents from Obsidian markdown to Org Roam while maintaining links."""
 
 import os
+import subprocess
+import re
 
 OUT_DIR = "./out/"
+# a list of tuples to find matches in the formatting string
+# this is for beginning of line items
+FORMAT_LIST = (
+    ["####", "###", "##", "---"],
+    ["***", "**", "*", ""],
+)
 
 
 def name_output(input_file: str) -> str:
@@ -13,9 +21,65 @@ def name_output(input_file: str) -> str:
     return output_md[0] + ".org"
 
 
+def replace_links(input_line: str) -> str:
+    """Return output line with properly formatted links."""
+    output_line = input_line
+    # Standard obsidian links
+    links = re.findall(r"\[\[(?:\S+\s*)+\]\]", output_line)
+    for link in links:
+        new_link = link.replace("[", "")
+        new_link = new_link.replace("]", "")
+        if "." in link:
+            new_link = f"[[./{new_link}][{new_link}]]"
+        else:
+            new_link = f"[[./{new_link}.org][{new_link}]]"
+        output_line = output_line.replace(link, new_link)
+    # Links with special descriptions
+    link_names = re.findall(r"\[\[(?:\w+\s*)+\|", output_line)
+    link_locations = re.findall(r"\|(?:\w+\s*)+\]\]", output_line)
+    try:
+        assert len(link_names) == len(link_locations)
+    except AssertionError:
+        print("Error: link names and locations do not match up.")
+    for i in range(len(link_names)):
+        new_link_name = link_names[i].replace("[", "").replace("|", "")
+        new_link_location = link_locations[i].replace("]", "").replace("|", "")
+        if "." in new_link_location:
+            new_link = f"[[./{new_link_location}][{new_link_name}]]"
+        else:
+            new_link = f"[[./{new_link_location}.org][{new_link_name}]]"
+        output_line = output_line.replace(link, new_link)
+    # Standard markdown links
+
+    return output_line
+
+
+def replace_code_block(input_line: str) -> str:
+    """Return properly formatted code blocks."""
+    code_block = re.search("```\w+", input_line)
+    output_line = input_line
+    if code_block is not None:
+        output_line = input_line.replace("```", "#+begin_src ")
+    else:
+        code_block = re.search("```", input_line)
+        if code_block is not None:
+            output_line = input_line.replace("```", "#+end_src")
+
+    return output_line
+
+
 def format_line(input_line: str) -> str:
     """Return a properly formatted line"""
-    return input_line
+    output_line = input_line
+    # start of line items
+    for item in range(len(FORMAT_LIST[0])):
+        if output_line.startswith(FORMAT_LIST[0][item]):
+            output_line = input_line.replace(FORMAT_LIST[0][item], FORMAT_LIST[1][item])
+
+    output_line = replace_links(output_line)
+    output_line = replace_code_block(output_line)
+
+    return output_line
 
 
 def transfer(input_file: str) -> None:
@@ -23,8 +87,8 @@ def transfer(input_file: str) -> None:
     output_file = name_output(input_file)
 
     try:
-        with open(input_file, "r") as md:
-            in_lines = md.readlines()
+        with open(input_file, "r") as markdown:
+            in_lines = markdown.readlines()
     except FileNotFoundError:
         print("Error: file not found.")
         return
@@ -33,8 +97,31 @@ def transfer(input_file: str) -> None:
     except FileExistsError:
         pass
     with open(OUT_DIR + output_file, "w") as out:
+        out.write(":PROPERTIES:\n:ID:       ")
+        try:
+            uuid = (
+                subprocess.run(
+                    ["/usr/bin/uuidgen"],
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.STDOUT,
+                )
+                .stdout.decode()
+                .strip()
+            )
+        except subprocess.CalledProcessError:
+            print("Error retrieving uuid.")
+            return
+        out.write(uuid)
+        out.write("\n:END:\n")
+        if in_lines[0].startswith("#"):
+            in_lines[0] = in_lines[0].replace("#", "#+title:")
         for line in in_lines:
+            if line.startswith("tags:"):
+                line = line.replace("#", "")
+                line = line.replace("tags:", "#+filetags:")
             out.write(format_line(line))
+    with open(OUT_DIR + ".orgids", "a") as append:
+        append.write(f'("Obsidian/{output_file}" "{uuid}")')
 
 
 if __name__ == "__main__":
